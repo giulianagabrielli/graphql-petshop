3ccb6acf15909e9ee83407b89fe5962bdfb90723
giulianagabrielli <glngabrielli@gmail.com>
2020-09-10 09:15:58 -0300

    crud cliente finalizado

--- infraestrutura/crud/cliente.js
+++ infraestrutura/crud/cliente.js
@@ -1,36 +1,40 @@
 const executaQuery = require('../database/queries')
 
 class Cliente {
-  lista(res) {
+  lista() {
     const sql = 'SELECT * FROM Clientes'
 
-    executaQuery(res, sql)
+    return executaQuery(sql)
   }
 
-  buscaPorId(res, id) {
+  buscaPorId(id) {
     const sql = `SELECT * FROM Clientes WHERE id=${id}`
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(clientes => clientes[0])
   }
 
-  adiciona(res, item) {
+  adiciona(item) {
     const { nome, cpf } = item
     const sql = `INSERT INTO Clientes(nome, CPF) VALUES('${nome}', '${cpf}')`
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(resposta => ({
+      id: resposta.insertId,
+      nome, 
+      cpf
+    }))
   }
 
-  atualiza(res, novoItem, id) {
-    const { nome, cpf } = novoItem
+  atualiza(novoItem) {
+    const { id, nome, cpf } = novoItem
     const sql = `UPDATE Clientes SET nome='${nome}', CPF='${cpf}' WHERE id=${id}`
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(() => novoItem)
   }
 
-  deleta(res, id) {
+  deleta(id) {
     const sql = `DELETE FROM Clientes WHERE id=${id}`
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(() => id)
   }
 }
 
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const customExpress = require('./config/custom-express')
-const conexao = require('./infraestrutura/conexao')
-const Tabelas = require('./infraestrutura/database/tabelas')
+const { GraphQLServer } = require('graphql-yoga') //npm install graphql-yoga
+const conexao = require('./infraestrutura/conexao') //conexao com o banco
+const Tabelas = require('./infraestrutura/database/tabelas') //criação de tabelas
 
-const app = customExpress()
+const Operacoes = require('./infraestrutura/operations')
 
 conexao.connect(erro => {
   if (erro) {
@@ -14,8 +14,24 @@ conexao.connect(erro => {
   Tabelas.init(conexao)
 })
 
-app.listen(4000, () => {
-  console.log('Servidor rodando na porta 4000')
+const Clientes = new Operacoes('cliente')
+
+const resolvers = {
+  Query: {
+    status: () => "Servidor rodando ",
+    clientes: () => Clientes.lista(),
+    cliente: (root, { id }) => Clientes.buscaPorId(id)
+  }, 
+  Mutation: {
+    adicionarCliente: (root, params) => Clientes.adiciona(params),
+    atualizarCliente: (root, params) => Clientes.atualiza(params),
+    deletarCliente: (root, { id }) => Clientes.deleta(id)
+  }
+}
+
+const servidor = new GraphQLServer({
+  resolvers,
+  typeDefs: './schema.graphql'
 })
 
-//npm install graphql-yoga 
+servidor.start(() => console.log('servidor ouvindo'))
